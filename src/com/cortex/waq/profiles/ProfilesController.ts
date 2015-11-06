ae3c4cdf29f98ec43a731c49fc50fec9e8a1334f
Etienne Bergeron <ebergeron@cortexmedia.ca>
2015-11-06 16:55:45 -0500

    Placeholder portraits

--- src/com/cortex/waq/profiles/ProfilesController.ts
+++ src/com/cortex/waq/profiles/ProfilesController.ts
@@ -1,3 +1,5 @@
+import ListComponent from "../../core/component/ListComponent";
+
 import MVCEvent from "../../core/mvc/event/MVCEvent";
 import EventDispatcher from "../../core/event/EventDispatcher";
 import AbstractView from "../../core/mvc/AbstractView";
@@ -7,6 +9,7 @@ import Profile from "./data/Profile"
 export default class ProfilesController extends EventDispatcher {
 
 	private mProfilesView:AbstractView;
+	private mListComponent:ListComponent;
 
 	private mProfiles:Array<Profile>;
 
@@ -20,12 +23,17 @@ export default class ProfilesController extends EventDispatcher {
 		this.mProfilesView.AddEventListener(MVCEvent.TEMPLATE_LOADED, this.OnTemplateLoaded, this);
 		this.mProfilesView.LoadTemplate("templates/profiles/profiles.html");
 
+		this.mListComponent = new ListComponent();
+
 		this.mProfiles = [];
 	}
 
 	public Destroy():void {
 		var scheduleHTMLElement:HTMLElement = document.getElementById("profiles-view");
 		document.getElementById("content-current").removeChild(scheduleHTMLElement);
+
+		this.mListComponent.Destroy();
+		this.mListComponent = null;
 
 		this.mProfilesView.Destroy();
 		this.mProfilesView = null;
@@ -35,13 +43,17 @@ export default class ProfilesController extends EventDispatcher {
 		document.getElementById("content-loading").innerHTML += this.mProfilesView.RenderTemplate({});
 		this.mProfilesView.RemoveEventListener(MVCEvent.TEMPLATE_LOADED, this.OnTemplateLoaded, this);
 		this.DispatchEvent(new MVCEvent(MVCEvent.TEMPLATE_LOADED));
+
+		this.mListComponent.Init("profiles-grid");
 		this.CreateProfileTiles();
 	}
 
 	private CreateProfileTiles():void {
-		var grid:HTMLElement = document.getElementById("profiles-grid");
 		for (var i:number = 0, iMax:number = 25; i < iMax; i++) {
-			grid.innerHTML += '<div class="profiles-tile" style="background-image: url(\'img/p1.png\');"></div>';
+			var n:number = Math.ceil(Math.random() * 8);
+			var p:Profile = new Profile(); p.photo = "p" + n + ".png";
+			var profileTileView:AbstractView = new AbstractView();
+			this.mListComponent.AddComponent(profileTileView, "templates/profiles/profileTile.html", p);
 		}
 	}
 }
