170de1314deaa481543125f50bacdc053ae16b05
Etienne Bergeron <ebergeron@cortexmedia.ca>
2015-11-30 08:31:43 -0500

    Schedule screen layout

--- src/com/cortex/waq/conference/ConferenceController.ts
+++ src/com/cortex/waq/conference/ConferenceController.ts
@@ -4,32 +4,32 @@ import Conference from "./data/Conference";
 import ConferenceModel from "./ConferenceModel";
 
 export default class ConferenceController extends EventDispatcher {
-	
+
 	private mConferences:Array<Conference>;
-	
+
 	constructor() {
 		super();
-		
+
 		this.Init();
 	}
-	
+
 	private Init():void {
-		this.mConferences = ConferenceModel.GetInstance().GetConferences();
+		//this.mConferences = ConferenceModel.GetInstance().GetConferences();
 	}
-	
+
 	public GetConferences():Array<Conference> {
 		return this.mConferences;
 	}
-	
+
 	public GetConferencesWithFilters(aFilterIds:Array<number>):void {
-		
+
 	}
-	
+
 	public GetConferencesForDate():void {
-		
+
 	}
-	
+
 	public GetConferencesOfSpeaker():void {
-		
+
 	}
 }
