8425b298998a58c72456162100d7b6bc6ec0aa24
Mathieu Rhéaume <mrheaume@cortexmedia.ca>
2016-01-26 12:15:00 -0500

    Hash JS Output on compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,49 @@
 var webpack = require('webpack');
 var WebpackNotifierPlugin = require('webpack-notifier');
+var HtmlPlugin = require('cortex-webpack-html-generator');
 
 module.exports = {
-	entry: './src/com/cortex/waq/main/Main.ts',
-	output: {
-		path: __dirname + "/www",
-		filename: 'app.js'
-	},
-	//devtool: 'source-map',
-	resolve: {
-        alias: {
-            "matches-selector/matches-selector": "desandro-matches-selector",
-            "eventEmitter/EventEmitter": "wolfy87-eventemitter",
-            "get-style-property/get-style-property": "desandro-get-style-property"
-        },
-		extensions: ['', 'lib/', '.webpack.js', '.web.js', '.ts', '.js']
-	},
-	plugins: [
-		new WebpackNotifierPlugin(),
-		new webpack.ProvidePlugin({
-		    Masonry: "masonry-layout"
-		}),
-		new webpack.ProvidePlugin({
-		    ImagesLoaded: "imagesloaded"
-		})
-	],
-	module: {
-		loaders: [
-			{
-				test: /masonry-layout/,
-				loader: 'imports?define=>false&this=>window'
-			},
-            {
-		        test: /imagesloaded/,
-		        loader: 'imports?define=>false&this=>window'
-			},
-			{
-				test: /\.ts$/,
-				loader: 'ts-loader'
-			}
-		]
-	}
+  entry: './src/com/cortex/waq/main/Main.ts',
+  output: {
+    path: __dirname + "/www",
+    filename: 'app_[hash].bundle.js'
+  },
+  //devtool: 'source-map',
+  resolve: {
+    alias: {
+      "matches-selector/matches-selector": "desandro-matches-selector",
+      "eventEmitter/EventEmitter": "wolfy87-eventemitter",
+      "get-style-property/get-style-property": "desandro-get-style-property"
+    },
+    extensions: ['', 'lib/', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  plugins: [
+    new WebpackNotifierPlugin(),
+    new webpack.ProvidePlugin({
+      Masonry: "masonry-layout"
+    }),
+    new webpack.ProvidePlugin({
+      ImagesLoaded: "imagesloaded"
+    }),
+    new HtmlPlugin({
+      template: 'assets/index.html.tmpl',
+      output_filename: 'index.html'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /masonry-layout/,
+        loader: 'imports?define=>false&this=>window'
+      },
+      {
+        test: /imagesloaded/,
+        loader: 'imports?define=>false&this=>window'
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
 };
