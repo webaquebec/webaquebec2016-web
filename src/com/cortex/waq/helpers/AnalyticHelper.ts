17dd62da67f43213f9f7e9a9eb03117fcd00c17c
David COHEN <dcohen@cortexmedia.ca>
2016-03-08 15:47:21 -0500

    Add page field on send track to google analytic

--- src/com/cortex/waq/helpers/AnalyticHelper.ts
+++ src/com/cortex/waq/helpers/AnalyticHelper.ts
@@ -2,7 +2,9 @@ export default class AnalyticHelper {
 
 	public static SendEvent():void {
 		if(window['ga'] !== undefined) {
-			window['ga']("send", "pageview");
+			window['ga']("send", "pageview", {
+				'page': location.pathname + location.search + location.hash
+			});
 		}
 	}
 
