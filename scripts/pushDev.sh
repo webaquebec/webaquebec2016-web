cf570cd7f1862e71dc3275ef21a071e36174c296
Jean-Denis Caron <jdcaron@cortex.bz>
2016-03-23 16:46:54 -0400

    Repo synchronization: Fix Google indexing issue of waq.cortex.ninja. 	new file:   env/dev/robots.txt 	modified:   scripts/pushDev.sh

--- scripts/pushDev.sh
+++ scripts/pushDev.sh
@@ -1,10 +1,13 @@
 #!bin/bash
 
+# NOTICE : Please do not add your prem file path, user your '.ssh/config' instead
 cd www
 zip -r waq.zip ./*
-scp -i /Users/mcote/Desktop/QuebecNumerique-WAQ.pem waq.zip ubuntu@54.86.106.29:/home/ubuntu
+zip -d waq.zip sitemap.xml
+zip -j waq.zip ../env/dev/robots.txt
+scp waq.zip ubuntu@waq.cortex.ninja:/home/ubuntu
 rm -rf waq.zip
-ssh -i /Users/mcote/Desktop/QuebecNumerique-WAQ.pem ubuntu@54.86.106.29 "sudo mv /home/ubuntu/waq.zip /var/www/waq/public_html/ &&
+ssh ubuntu@waq.cortex.ninja "sudo mv /home/ubuntu/waq.zip /var/www/waq/public_html/ &&
 																			cd /var/www/waq/public_html &&
 																			sudo unzip waq.zip &&
 																			sudo rm -rf waq.zip"
