eccfa07d16a3e15ce53af7d7f90373455ebcaad0
Jean-Denis Caron <jdcaron@cortex.bz>
2016-03-31 14:54:09 -0400

    Fix speaker back button issue when you click on a speaker and go back to all the speakers. 	modified:   src/com/cortex/waq/main/Main.ts

--- src/com/cortex/waq/main/Main.ts
+++ src/com/cortex/waq/main/Main.ts
@@ -451,7 +451,7 @@ export default class Main extends EventDispatcher implements IKeyBindable {
 
 	private SetupNavigable(aName:string, aControllerClass:any):void {
 
-		if (aName === this.mCurrentAction || this.mAnimationController.IsAnimating) { return };
+		if (this.mAnimationController.IsAnimating) { return };
 
 		EConfig.CURRENT_PATH = aName;
 
